96674151baad45e438026e0736fed82c617348ce
Tobias Laatsch <tobias.laatsch@uni-duesseldorf.de>
2013-05-29 20:13:44 +0200

    small change to auto_install.sh

--- webpcap/auto_install.sh
+++ webpcap/auto_install.sh
@@ -4,21 +4,22 @@
 #
 # Note: If you don't use sudo, comment the last line and run make install as root.
 
-git clone git://github.com/nori0428/mod_websocket.git
-wget http://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.32.tar.gz
-tar xvf ./lighttpd-1.4.32.tar.gz
+git clone git://github.com/nori0428/mod_websocket.git &&
+wget http://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.32.tar.gz &&
+tar xvf ./lighttpd-1.4.32.tar.gz &&
 
-cd ./mod_websocket
-./bootstrap
-./configure --with-websocket=RFC-6455 --with-test
-make clean check
+cd ./mod_websocket &&
+./bootstrap &&
+./configure --with-websocket=RFC-6455 --with-test &&
+make clean check &&
 
-./bootstrap
-./configure --with-lighttpd=../lighttpd-1.4.32
-make install
+./bootstrap &&
+./configure --with-lighttpd=../lighttpd-1.4.32 &&
+make install &&
 
-cd ../lighttpd-1.4.32
-./autogen.sh
-./configure --with-websocket=RFC-6455
-make
+cd ../lighttpd-1.4.32 &&
+./autogen.sh &&
+./configure --with-websocket=RFC-6455 &&
+make &&
 sudo make install
+
